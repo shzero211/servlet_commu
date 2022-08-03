d6f85614d7f4f75e564387b22299a1046eac881e
SangHun <shzero211@naver.com>
2022-08-03 23:12:09 +0900

    Servlet dependency 추가 및

---
+++ src/main/java/com/ll/commu/DispatchServlet.java
@@ -0,0 +1,16 @@
+package com.ll.commu;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/usr/*")
+public class DispatchServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.getWriter().append("Work?");
+    }
+}
