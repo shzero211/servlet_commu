3fe1b2ecef0d56b5de9071430e04eca9dc7a0490
SangHun <shzero211@naver.com>
2022-08-04 02:41:50 +0900

    ArticleRepository 안에 테스트데이터 생성메서드 만들고 필요한 변수,메서드를 static 으로 변경

--- src/main/java/com/ll/commu/article/ArticleRepository.java
+++ src/main/java/com/ll/commu/article/ArticleRepository.java
@@ -4,16 +4,18 @@ import com.ll.commu.article.dto.ArticleDto;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class ArticleRepository {
-    private List<ArticleDto> articles;
-    int id;
-    public ArticleRepository(){
+    private static List<ArticleDto> articles;
+    static int id;
+    static {
         articles=new ArrayList<>();
         id=0;
+        makeTestData();
     }
     //articles 에 save
-    public long save(String name,String content){
+    public static long save(String name,String content){
         ArticleDto articleDtoNew=new ArticleDto();
         articleDtoNew.setId(++id);
         articleDtoNew.setTitle(name);
@@ -33,5 +35,13 @@ public class ArticleRepository {
             }
         }
         return null;
+    }
+    public static  void makeTestData(){
+        IntStream.rangeClosed(1,10).forEach(id->{
+            String title="제목%d".formatted(id);
+            String content="내용%d".formatted(id);
+            save(title,content);
+
+        });
     }
 }
