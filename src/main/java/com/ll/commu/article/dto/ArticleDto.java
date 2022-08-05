53da9824e049b2ad05ae7d9af7c29640d5bb550c
SangHun <shzero211@naver.com>
2022-08-06 01:41:10 +0900

    Feat:Jackson라이브러리 추가,Jackson을 이용한 json<->String 변환

--- src/main/java/com/ll/commu/article/dto/ArticleDto.java
+++ src/main/java/com/ll/commu/article/dto/ArticleDto.java
@@ -1,16 +1,32 @@
 package com.ll.commu.article.dto;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.*;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class ArticleDto {
     private long id;
     private String title;
     private String content;
+
+    public String toJson() {
+    ObjectMapper om=new ObjectMapper();
+        try {
+            return om.writeValueAsString(this);
+        } catch (JsonProcessingException e) {
+           return "";
+        }
+    }
+    public ArticleDto toObj(String json){
+        ObjectMapper om=new ObjectMapper();
+        try {
+            return om.readValue(json,ArticleDto.class);
+        } catch (JsonProcessingException e) {
+            return null;
+        }
+    }
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,11 +1,27 @@
+import com.ll.commu.article.dto.ArticleDto;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class AppTest {
     @Test
     public void 테스트_assertThat(){
         int rs=10+20;
-        Assertions.assertEquals(rs,30);
+        assertEquals(rs,30);
+    }
+    @Test
+    public void ArticleDto_를_json_타입으로변경(){
+        ArticleDto articleDto=new ArticleDto(1,"title1","content1");
+        String json=articleDto.toJson();
+        assertEquals(json, "{\"id\":1,\"title\":\"title1\",\"content\":\"content1\"}");
+    }
 
+    @Test
+    public void JSON_을_ArticleDto타입으로_변경(){
+        ArticleDto articleDto=new ArticleDto(1,"title1","content1");
+
+        ArticleDto articleDto2=articleDto.toObj(articleDto.toJson());
+        assertEquals(articleDto.toString(),articleDto2.toString());
     }
 }
